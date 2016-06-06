b5e59ad6b66bd0e7f05d00a83beea1d8b332eb2e
kagos <kristin.agos@yahoo.com>
2016-06-05 21:34:18 -0500

    Navbar established + Search added to navbar + Navbar mobile formatting set + Name change + City in Cities to create a list

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,45 +1,49 @@
 angular.module('weather', [])
   .controller('WeatherController', ['$scope', '$http', function($scope, $http) {
     var initializing = true;
+    $scope.cities = {};
     $scope.$watch('search', function(val) {
       if (initializing) {
         setTimeout(function() { initializing = false; }, 0);
       } else {
-        $scope.icon = "assets/spinner.gif";
         fetchCityState(val);
+        $scope.search = "";
       }
     }
   );
 
-    var fetchCityState = function(zip) {
-      if(zip)
-        zip.length === 5 ?
-          $http.get('http://maps.googleapis.com/maps/api/geocode/json?address=' + zip)
-            .then(function(lookupObj) {
-              fetchWeather(lookupObj);
-            })
-            .catch(function (err) {
-              console.error('Error in fetchCityState');
-            }) :
-        console.error('Invalid zip!');
-    };
+  var fetchCityState = function(zip) {
+    if(zip)
+      zip.length === 5 ?
+        $http.get('http://maps.googleapis.com/maps/api/geocode/json?address=' + zip)
+          .then(function(lookupObj) {
+            fetchWeather(lookupObj);
+          })
+          .catch(function (err) {
+            console.error('Error in fetchCityState');
+          }) :
+      console.error('Invalid zip!');
+  };
 
-    var fetchWeather = function(lookupObj) {
-      var formatted_address = lookupObj.data.results[0].formatted_address;
-      var addressArr = lookupObj.data.results[0].formatted_address.split(', ');
-      var zipPosition = addressArr[1].lastIndexOf(' ');
-      var stateAbbr = addressArr[1].substring(0, zipPosition);
-      var city = addressArr[0].replace(' ', '_');
+  var fetchWeather = function(lookupObj) {
+    var formatted_address = lookupObj.data.results[0].formatted_address;
+    var addressArr = lookupObj.data.results[0].formatted_address.split(', ');
+    var zipPosition = addressArr[1].lastIndexOf(' ');
+    var zip = addressArr[1].substring(zipPosition, addressArr[1].length);
+    var stateAbbr = addressArr[1].substring(0, zipPosition);
+    var city = addressArr[0].replace(' ', '_');
 
-      $http.get('http://api.wunderground.com/api/4fff4d1fab37ffe7/geolookup/conditions/q/' + stateAbbr + '/' + city + '.json')
-        .then(function(response) {
-          $scope.formatted_address = formatted_address;
-          $scope.weather = response.data.current_observation.weather;
-          $scope.temperature = response.data.current_observation.temperature_string;
-          $scope.icon = "http://icons.wxug.com/i/c/k/" + response.data.current_observation.icon + ".gif";
-        })
-        .catch(function (err) {
-          console.error('Error in fetchWeather');
-        });
+    $http.get('http://api.wunderground.com/api/4fff4d1fab37ffe7/geolookup/conditions/q/' + stateAbbr + '/' + city + '.json')
+      .then(function(response) {
+        $scope.cities[zip] = {
+          formatted_address: formatted_address,
+          weather: response.data.current_observation.weather,
+          temperature: response.data.current_observation.temperature_string,
+          icon: "http://icons.wxug.com/i/c/k/" + response.data.current_observation.icon + ".gif"
+        };
+      })
+      .catch(function (err) {
+        console.error('Error in fetchWeather');
+      });
     };
   }]);
