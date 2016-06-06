e03db4436c2b1e3e5ff778b46be0cd09af1c34bf
kagos <kristin.agos@yahoo.com>
2016-06-05 21:34:18 -0500

    + added Google zip lookup (returns city/state) + configured wunderground api call to use Google zip lookup results + updated index.html model data
    ++++ WORKS ++++

--- src/js/weather.js
+++ src/js/weather.js
@@ -2,16 +2,41 @@
 
 angular.module("weather", [])
   .controller("WeatherController", function($scope, $http) {
-    $scope.$watch("search", function() {
+    let initializing = true;
+    $scope.$watch("search", function(val) {
       if (initializing) {
-        $timeout(function() { initializing = false; });
+        setTimeout(function() { initializing = false; }, 0);
       } else {
-        fetchWeather();
+        fetchCityState(val);
       }
     });
 
-    function fetchWeather(){
-      $http.get("http://api.wunderground.com/api/4fff4d1fab37ffe7/geolookup/conditions/q/IA/Cedar_Rapids.json")
-        .then(function(response) { $scope.details = response.data; });
-    }
+    var fetchCityState = (zip) => {
+      if(zip)
+        zip.length === 5 ?
+          $http.get("http://maps.googleapis.com/maps/api/geocode/json?address=" + zip)
+            .then(function (data) {
+              fetchWeather(data);
+            })
+            .catch(function (err) {
+              console.error("Error in fetchCityState");
+            }) :
+        console.error("Invalid zip!");
+    };
+
+    var fetchWeather = (lookupObj) => {
+      const addressArr = lookupObj.data.results[0].formatted_address.split(", ");
+      const zipPosition = addressArr[1].lastIndexOf(" ");
+      const stateAbbr = addressArr[1].substring(0, zipPosition);
+      const city = addressArr[0].replace(" ", "_");
+
+      $http.get("http://api.wunderground.com/api/4fff4d1fab37ffe7/geolookup/conditions/q/" + stateAbbr + "/" + city + ".json")
+        .then(function(response) {
+          console.log(response.data.current_observation);
+          $scope.current_observation = response.data.current_observation;
+        })
+        .catch(function (err) {
+          console.error("Error in fetchWeather");
+        });
+    };
   });
